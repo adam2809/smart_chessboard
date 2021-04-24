270a8a9fa5c5dcc60e2ddc9011290c4d4058223e
Adam Kulesza <adam.kule@gmail.com>
2021-04-24 15:39:34 +0200

    Object-oriented refactor

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "driver/touch_pad.h"
 #include "esp_log.h"
 #include <driver/adc.h>
+#include <string>
+#include "esp_task_wdt.h"
 
 #define TOUCH_PAD_NO_CHANGE   (-1)
 #define TOUCH_THRESH_NO_USE   (0)
@@ -12,24 +14,48 @@
 
 gpio_config_t io_conf;
 
-constexpr uint8_t touch_pads_count = 4;
-constexpr touch_pad_t touch_pads[touch_pads_count] = {TOUCH_PAD_NUM4,TOUCH_PAD_NUM5,TOUCH_PAD_NUM8,TOUCH_PAD_NUM9};
-constexpr int thresh_vals[touch_pads_count] = {600,640,680,695};
-constexpr gpio_num_t led_pins[touch_pads_count] = {GPIO_NUM_4,GPIO_NUM_18,GPIO_NUM_19,GPIO_NUM_23};
-
-
 class touch_pad{
-    public:
-        touch_pad(const touch_pad_t ch,const int th,const gpio_num_t lp) : 
-            channel(ch),
-            thresh(th),
-            led_pin(lp){
-            touch_pad_config(ch,TOUCH_THRESH_NO_USE);
-        }
     private:
         const touch_pad_t channel;
         const int thresh;
         const gpio_num_t led_pin;
+
+    public:
+        touch_pad(const touch_pad_t ch,const int th,const gpio_num_t lp) : 
+            channel(ch),
+            thresh(th),
+            led_pin(lp)
+        {
+        }
+
+        void init(){
+            touch_pad_config(channel,TOUCH_THRESH_NO_USE);
+        }
+
+        touch_pad_t get_channel(){
+            return channel;
+        }
+
+        int get_thresh(){
+            return thresh;
+        }
+
+        int get_led_pin(){
+            return led_pin;
+        }
+
+        bool check_thresh(){
+            uint16_t touch_value;
+            touch_pad_read_filtered(channel, &touch_value);
+            gpio_set_level(led_pin,touch_value < thresh);
+            return touch_value < thresh;
+        }
+
+        void print_info() {
+            uint16_t touch_value;
+            ESP_ERROR_CHECK(touch_pad_read_filtered(channel, &touch_value));
+            printf("T%d:[%d] THESH:[%d] | ", channel, touch_value,thresh);
+        }
 };
 
 namespace touch_pads{
@@ -45,73 +71,52 @@ namespace touch_pads{
  Print out values in a loop on a serial monitor.
  */
 static void tp_example_read_task(void *pvParameter){
-    uint16_t touch_filter_value;
     while (1) {
-        bool touched[touch_pads_count] = { false };
-        for (int i = 0; i < touch_pads_count; i++) {
-            touch_pad_read_filtered(touch_pads[i], &touch_filter_value);
-            printf("T%d:[%d] THESH:[%d] | ", touch_pads[i], touch_filter_value,thresh_vals[i]);
-            if(touch_filter_value < thresh_vals[i]){
-                touched[i] = true;
-            }
+        for(int i = 0; i < touch_pads::count; i++){
+            touch_pads::all[i].print_info();
         }
-        printf(" Touched:");
-        for (int i = 0; i < touch_pads_count; i++) {
-            gpio_set_level(led_pins[i],touched[i]);
-            if(touched[i]){
+        printf("Touched:");
+        for(int i = 0; i < touch_pads::count; i++){
+            if(touch_pads::all[i].check_thresh()){
                 printf("%d,",i);
             }
         }
         printf("\n");
+        esp_task_wdt_reset();
         vTaskDelay(200 / portTICK_PERIOD_MS);
     }
 }
 
-static void all_touch_pad_init(void)
-{
-    for (int i = 0;i < touch_pads_count;i++) {
-        touch_pad_config(touch_pads[i],TOUCH_THRESH_NO_USE);
-    }
-}
-
 static void all_led_pin_init(void){
-    //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
 
     uint64_t pin_select_mask = 0;
-    for(int i=0;i<touch_pads_count;i++){
-        pin_select_mask |= (1ULL<<led_pins[i]);
+    for(int i=0;i<touch_pads::count;i++){
+        pin_select_mask |= (1ULL<<touch_pads::all[i].get_led_pin());
     }
-
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
     io_conf.pin_bit_mask = pin_select_mask;
-    
-    //disable pull-down mode
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    //disable pull-up mode
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    //configure GPIO with the given settings
     gpio_config(&io_conf);
 }
 
+void all_touch_pads_init(){
+    for(int i=0;i<touch_pads::count;i++){
+        touch_pads::all[i].init();
+    }
+}
+
 extern "C" 
-void app_main(void)
-{
-    // Initialize touch pad peripheral.
-    // The default fsm mode is software trigger mode.
+void app_main(void){
     ESP_ERROR_CHECK(touch_pad_init());
-    // Set reference voltage for charging/discharging
-    // In this case, the high reference valtage will be 2.7V - 1V = 1.7V
-    // The low reference voltage will be 0.5
-    // The larger the range, the larger the pulse count value.
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-    all_touch_pad_init();
     touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
+    all_touch_pads_init();
 
     all_led_pin_init();
 
-    // Start task to read values sensed by pads
+
+
     xTaskCreate(&tp_example_read_task, "touch_pad_read_task", 2048, NULL, 5, NULL);
 }
